02cc18912e2adad47aa45c8513cbdeae07add9e3
karl-chan <karlwayne_chan@yahoo.com>
2018-03-30 10:53:04 +0100

    Defend against null returns

--- fund-analyser-app/src/utils/fund-utils.js
+++ fund-analyser-app/src/utils/fund-utils.js
@@ -7,7 +7,7 @@ const periods = ['5Y', '3Y', '1Y', '6M', '3M', '1M', '2W', '1W', '3D', '1D']
 
 export default {
   calcSummaryStats (summary) {
-    const periodReturns = fromPairs(periods.map(period => [period, summary.map(row => row.returns[period]).filter(isFinite)]))
+    const periodReturns = fromPairs(periods.map(period => [period, summary.map(row => row.returns && row.returns[period]).filter(isFinite)]))
     const maxReturns = fromPairs(periods.map(period => [period, max(periodReturns[period])]))
     const minReturns = fromPairs(periods.map(period => [period, min(periodReturns[period])]))
     const meanReturns = fromPairs(periods.map(period => [period, (maxReturns[period] + minReturns[period]) / 2]))
@@ -25,10 +25,10 @@ export default {
     if (summary && summary.length) {
       const { minReturns, maxReturns } = this.calcSummaryStats(summary)
       summary = summary.map(row => {
-        const scores = fromPairs(Object.entries(row.returns).map(([period, ret]) => {
+        const scores = row.returns ? fromPairs(Object.entries(row.returns).map(([period, ret]) => {
           const score = ret > 0 ? ret / maxReturns[period] : (ret < 0 ? -ret / minReturns[period] : ret)
           return [period, score]
-        }))
+        })) : {}
         return { ...row, ...{ metadata: { scores } } }
       })
     }
