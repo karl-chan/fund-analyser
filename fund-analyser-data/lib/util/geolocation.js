0d6a548740d0e437afd12bad907e928e7d901d07
karl-chan <karlwayne_chan@yahoo.com>
2019-04-26 07:39:21 +0100

    Don't retry ip geolocation if service unreliable

--- fund-analyser-data/lib/util/geolocation.js
+++ fund-analyser-data/lib/util/geolocation.js
@@ -3,19 +3,31 @@ module.exports = {
 }
 
 const Http = require('./http')
+const log = require('./log')
 
-const http = new Http()
+const http = new Http({
+    maxAttempts: 1
+})
 
 async function getLocationByIp (ip) {
     if (!ip) {
         return {}
     }
     const url = `http://ip-api.com/json/${ip}`
-    const { body } = await http.asyncGet(url)
-    const data = JSON.parse(body)
-    return {
-        city: data.city,
-        region: data.regionName,
-        country: data.country
+    try {
+        const { body } = await http.asyncGet(url)
+        const data = JSON.parse(body)
+        return {
+            city: data.city,
+            region: data.regionName,
+            country: data.country
+        }
+    } catch (err) {
+        log.warn('Failed to geolocate ip address: %s. Cause: %s', ip, err.stack)
+        return {
+            city: undefined,
+            region: undefined,
+            country: undefined
+        }
     }
 };
