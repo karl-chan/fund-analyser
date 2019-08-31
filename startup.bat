d259cb63c54089eece53e8f36820ca2fe999aa78
karl-chan <karlwayne_chan@yahoo.com>
2019-08-31 19:45:19 +0100

    Remove nginx from startup.bat

--- startup.bat
+++ startup.bat
@@ -1,4 +1,3 @@
-start cmd /c "start-nginx"
 start cmd /k "cd fund-analyser-app && quasar dev"
 start cmd /k "cd fund-analyser-data && npm run server.dev"
 start cmd /k "cd fund-analyser-compute && pipenv run python -m server.server"
