e24258daf00d03c3985e25aeb25fe45a96fd85da
karl-chan <karlwayne_chan@yahoo.com>
2020-01-03 23:08:05 +0000

    Fix target returns

--- fund-analyser-compute/lib/simulate/strategy/target_returns.py
+++ fund-analyser-compute/lib/simulate/strategy/target_returns.py
@@ -23,16 +23,10 @@ class TargetReturns(Strategy):
         isins = row.index[row].tolist()
         return isins
 
-    def _with_global_uptrend(self, dt: date) -> List[str]:
-        row = self.pos_gradient.loc[dt, :]
-        isins = row.index[row].tolist()
-        return isins
-
     @overrides
     def run(self, dt: date, prices_df: pd.DataFrame, fees_df: pd.DataFrame) -> List[str]:
         return list(set.intersection(
-            set(self._with_upside(dt)),
-            set(self._with_global_uptrend(dt))
+            set(self._with_upside(dt))
         ))
 
     @overrides
@@ -41,10 +35,6 @@ class TargetReturns(Strategy):
         lower_channel, upper_channel = price_channels(data.prices_df, timeperiod=time_period_1m)
         upsides = (upper_channel - data.prices_df) / data.prices_df
         self.has_upside = upsides.ge(self._target_returns)
-
-        smoothed_prices_1m = data.prices_df.rolling(time_period_1m).mean()
-        global_gradient = smoothed_prices_1m.pct_change()
-        self.pos_gradient = global_gradient.ge(0)
 
 
 if __name__ == "__main__":
