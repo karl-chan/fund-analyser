d245e74d80c66db03b1b6ebc9a6515f314411dd8
Karl Chan <karl-chan@bitbucket.org>
2020-02-04 21:47:09 +0000

    Fix Charles Stanley login aftter UI change

--- fund-analyser-data/lib/auth/CharlesStanleyDirectAuth.js
+++ fund-analyser-data/lib/auth/CharlesStanleyDirectAuth.js
@@ -7,10 +7,11 @@ const http = new Http()
 
 class CharlesStanleyDirectAuth {
     constructor () {
-        this.accountUrl = 'https://www.charles-stanley-direct.co.uk/Account'
-        this.checkMemorableWordUrl = 'https://www.charles-stanley-direct.co.uk/Account/CheckMemorableWord'
-        this.accountLoginChecksUrl = 'https://www.charles-stanley-direct.co.uk/Account/AccountLoginChecks'
-        this.myDirectAccountsUrl = 'https://www.charles-stanley-direct.co.uk/My_Dashboard/My_Direct_Accounts'
+        this.homeUrl = 'https://www.charles-stanley-direct.co.uk/'
+        this.loginUrl = 'https://www.charles-stanley-direct.co.uk/LoginApi/Login'
+        this.getMemorableWordUrl = 'https://www.charles-stanley-direct.co.uk/LoginApi/GetMemorableWord'
+        this.validateMemorableWordUrl = 'https://www.charles-stanley-direct.co.uk/LoginApi/ValidateMemorableWord'
+        this.logUserInUrl = 'https://www.charles-stanley-direct.co.uk/LoginApi/LogUserIn'
         this.myAccountsUrl = 'https://www.charles-stanley-direct.co.uk/Dashboard/MyAccounts'
 
         this.investmentSummaryUrl = 'https://www.charles-stanley-direct.co.uk/Dashboard/GetInvestmentSummaryJson?showByType='
@@ -29,8 +30,8 @@ class CharlesStanleyDirectAuth {
         // Step 2: Memorable word
         await this._checkMemorableWord(memorableWord, { csrfToken, jar })
 
-        // Step 3: Account login checks
-        await this._accountLoginChecks({ jar })
+        // Step 3: Log user in
+        await this._logUserIn({ csrfToken, jar })
 
         // Step 4: Get user name
         const { name } = await this._getMyAccount({ jar })
@@ -48,63 +49,74 @@ class CharlesStanleyDirectAuth {
 
     async _enterUserAndPass (user, pass, { jar }) {
         log.debug('Entering user and pass')
-        const { body: b1 } = await http.asyncGet(this.accountUrl, { jar })
+        const { body: b1 } = await http.asyncGet(this.homeUrl, { jar })
         const $1 = cheerio.load(b1)
-        const csrfToken1 = $1('input[name="__RequestVerificationToken"]').val()
+        const csrfToken = $1('#__AjaxAntiForgeryForm > input[name="__RequestVerificationToken"]').val()
 
-        const { headers: h2 } = await http.asyncPost(this.accountUrl,
-            {
-                jar,
-                form: {
-                    'LoginId': user,
-                    'Password': pass,
-                    '__RequestVerificationToken': csrfToken1
+        try {
+            await http.asyncPost(this.loginUrl,
+                {
+                    jar,
+                    form: {
+                        'Username': user,
+                        'Password': pass,
+                        '__RequestVerificationToken': csrfToken
+                    }
                 }
-            }
-        )
-        if (!this.checkMemorableWordUrl.includes(h2.location)) {
+            )
+        } catch (err) {
             throw new Error('Incorrect username or password')
         }
-
-        const { body: b3 } = await http.asyncGet(this.checkMemorableWordUrl, { jar })
-        const $3 = cheerio.load(b3)
-        const csrfToken3 = $3('input[name="__RequestVerificationToken"]').val()
         log.debug('User and pass accepted')
-        return { csrfToken: csrfToken3 }
+        return { csrfToken }
     }
 
     async _checkMemorableWord (memorableWord, { csrfToken, jar }) {
         log.debug('Checking memorable word')
-        const { body: b1 } = await http.asyncGet(this.checkMemorableWordUrl, { jar })
-        const $1 = cheerio.load(b1)
-        const charIndex1 = parseInt($1('#memorable-word-form > div.field > div > strong:nth-child(1)').text())
-        const charIndex2 = parseInt($1('#memorable-word-form > div.field > div > strong:nth-child(3)').text())
-        const charIndex3 = parseInt($1('#memorable-word-form > div.field > div > strong:nth-child(5)').text())
 
-        const { headers: h2 } = await http.asyncPost(this.checkMemorableWordUrl,
-            {
-                jar,
-                form: {
-                    '__RequestVerificationToken': csrfToken,
-                    'Character1': memorableWord.charAt(charIndex1 - 1),
-                    'Character2': memorableWord.charAt(charIndex2 - 1),
-                    'Character3': memorableWord.charAt(charIndex3 - 1)
-                }
+        const { body: b1 } = await http.asyncPost(this.getMemorableWordUrl, {
+            jar,
+            headers: {
+                'RequestVerificationToken': csrfToken,
+                'X-Requested-With': 'XMLHttpRequest'
             }
-        )
-        if (!this.accountLoginChecksUrl.includes(h2.location)) {
+        })
+        const { FirstCharacterPosition, SecondCharacterPosition, ThirdCharacterPosition } = JSON.parse(b1)
+        try {
+            await http.asyncPost(this.validateMemorableWordUrl,
+                {
+                    jar,
+                    headers: {
+                        'RequestVerificationToken': csrfToken,
+                        'X-Requested-With': 'XMLHttpRequest'
+                    },
+                    form: {
+                        'firstCharacter': memorableWord.charAt(FirstCharacterPosition - 1),
+                        'secondCharacter': memorableWord.charAt(SecondCharacterPosition - 1),
+                        'thirdCharacter': memorableWord.charAt(ThirdCharacterPosition - 1)
+                    }
+                }
+            )
+        } catch (err) {
             throw new Error('Incorrect memorable word')
         }
         log.debug('Memorable word accepted')
     }
 
-    async _accountLoginChecks ({ jar }) {
-        log.debug('Performing account login check')
-        const { headers } = await http.asyncGet(this.accountLoginChecksUrl, { jar, followRedirect: false })
-        if (!this.myDirectAccountsUrl.includes(headers.location)) {
-            throw new Error('Account login checks failed')
+    async _logUserIn ({ csrfToken, jar }) {
+        log.debug('Logging user in')
+        try {
+            await http.asyncPost(this.logUserInUrl, {
+                jar,
+                headers: {
+                    'RequestVerificationToken': csrfToken,
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            })
+        } catch (err) {
+            throw new Error('Log user in failed')
         }
-        log.debug('Account login check completed')
+        log.debug('Log user in completed')
     }
 
     async _getMyAccount ({ jar }) {
