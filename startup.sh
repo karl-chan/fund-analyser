4a01fe462e07fe82db2f950629289fb3e85d8866
Karl Chan <karl-chan@bitbucket.org>
2020-02-15 22:24:32 +0000

    Modify startup script to open terminals in new tabs

--- startup.sh
+++
@@ -1,9 +0,0 @@
-osascript -e 'tell app "Terminal"
-    do script "cd '"$(pwd)"'/fund-analyser-app && quasar dev -m pwa"
-end tell'
-osascript -e 'tell app "Terminal"
-    do script "cd '"$(pwd)"'/fund-analyser-data && npm run server.dev"
-end tell'
-osascript -e 'tell app "Terminal"
-    do script "cd '"$(pwd)"'/fund-analyser-compute && pipenv run python -m server.server"
-end tell'
