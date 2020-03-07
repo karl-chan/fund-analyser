a7b11b1ddb2d1526b8bea996a08fcd12623c6628
Karl Chan <karl-chan@bitbucket.org>
2020-03-07 21:04:04 +0000

    Fix afterFeesReturn bug

--- fund-analyser-compute/lib/indicators/after_fees_return.py
+++ fund-analyser-compute/lib/indicators/after_fees_return.py
@@ -18,6 +18,6 @@ class AfterFeesReturn(Indicator):
         isin = fund.isin
         fees_df = calc_fees([fund])
         fees_per_year = fees_df.at[isin, "total_one_off_fees"] + fees_df.at[isin, "total_annual_fees"]
-        returns_per_year = fund.returns.get("1Y", np.nan)
+        returns_per_year = fund.returns.get("1Y") or np.nan
         after_fees_return = returns_per_year - fees_per_year
         return FundIndicator(after_fees_return)
