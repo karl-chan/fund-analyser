e7d5d9ed0d9d03e6c2af9e826f0e8c514012de64
Karl Chan <karl-chan@bitbucket.org>
2020-05-14 23:06:42 +0100

    Working support resistance algo

--- fund-analyser-compute/lib/indicators/indicator_utils.py
+++ fund-analyser-compute/lib/indicators/indicator_utils.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+from datetime import date
+from typing import List, Tuple
 
 import numpy as np
 import pandas as pd
@@ -51,27 +52,75 @@ def ppo(prices_df: pd.DataFrame, fast=12, slow=26, signal=9) -> Tuple[pd.DataFra
 
 
 def support_resistance(prices_df: pd.DataFrame) -> \
-        Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
-    # 1 = support, 0 = none, -1 = resistance
-    signs = np.sign(prices_df.diff().replace(0, np.nan)).ffill(axis=0).bfill(axis=0)
-    sr = np.sign(signs.diff().shift(-1)).fillna(0).astype("int")
+        Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    prev_support_dates, prev_support_prices, prev_resistance_dates, prev_resistance_prices = [], [], [], []
+    for isin, series in prices_df.items():
+        prev_support_dates_series, prev_support_prices_series, prev_resistance_dates_series, prev_resistance_prices_series = support_resistance_series(
+            series)
+        prev_support_dates.append(prev_support_dates_series)
+        prev_support_prices.append(prev_support_prices_series)
+        prev_resistance_dates.append(prev_resistance_dates_series)
+        prev_resistance_prices.append(prev_resistance_prices_series)
+    return pd.concat(prev_support_dates, axis=1), \
+           pd.concat(prev_support_prices, axis=1), \
+           pd.concat(prev_resistance_dates, axis=1), \
+           pd.concat(prev_resistance_prices, axis=1)
 
-    def prev_support_or_resistance_dates(sr: pd.DataFrame, is_support=True) -> pd.DataFrame:
-        target = 1 if is_support else -1
-        dates = pd.concat([sr.index.to_series()] * len(sr.columns), axis=1)
-        dates.columns = sr.columns
-        dates[sr != target] = np.nan
-        return dates.ffill()
 
-    def prev_dates_to_prices(dates: pd.DataFrame) -> pd.DataFrame:
-        return dates.apply(lambda col: col.map(prices_df[col.name]))
+def support_resistance_series(prices_series: pd.Series) -> \
+        Tuple[pd.Series, pd.Series, pd.Series, pd.Series]:
+    seen_supports: List[Tuple[date, float]] = []  # [(support date, support price)]
+    seen_resistances: List[Tuple[date, float]] = []  # [(resistance date, resistance price)]
+    prev_direction = 0  # 0 = flat, 1 = up, -1 = down
 
-    prev_support_dates = prev_support_or_resistance_dates(sr, is_support=True)
-    prev_support_prices = prev_dates_to_prices(prev_support_dates)
-    prev_resistance_dates = prev_support_or_resistance_dates(sr, is_support=False)
-    prev_resistance_prices = prev_dates_to_prices(prev_resistance_dates)
+    prev_support_dates = pd.Series(index=prices_series.index, name=prices_series.name, dtype="datetime64[ns]")
+    prev_support_prices = pd.Series(index=prices_series.index, name=prices_series.name)
+    prev_resistance_dates = pd.Series(index=prices_series.index, name=prices_series.name, dtype="datetime64[ns]")
+    prev_resistance_prices = pd.Series(index=prices_series.index, name=prices_series.name)
 
-    return sr, prev_support_dates, prev_support_prices, prev_resistance_dates, prev_resistance_prices
+    def _get_direction(prev_price: float, price: float) -> int:
+        if price > prev_price:
+            return 1
+        if price < prev_price:
+            return -1
+        return 0
+
+    def _get_prev_support(prev_price: float) -> Tuple[date, float]:
+        return next(
+            (t for t in reversed(seen_supports) if t[1] < prev_price),
+            (pd.NaT, np.nan)
+        )
+
+    def _get_prev_resistance(prev_price: float) -> Tuple[date, float]:
+        return next(
+            (t for t in reversed(seen_resistances) if t[1] > prev_price),
+            (pd.NaT, np.nan)
+        )
+
+    for iloc in range(1, len(prices_series)):
+        prev_date = prices_series.index[iloc - 1]
+        prev_price = prices_series.iat[iloc - 1]
+        price = prices_series.iat[iloc]
+        direction = _get_direction(prev_price, price)
+
+        # find last seen support / resistance
+        prev_support_dates[prev_date], prev_support_prices[prev_date] = _get_prev_support(prev_price)
+        prev_resistance_dates[prev_date], prev_resistance_prices[prev_date] = _get_prev_resistance(prev_price)
+
+        # update seen supports and resistances (expanding window)
+        if direction != 0:
+            if direction == 1 and prev_direction == -1:
+                seen_supports.append((prev_date, prev_price))
+            elif direction == -1 and prev_direction == 1:
+                seen_resistances.append((prev_date, prev_price))
+            prev_direction = direction
+
+    # need to handle last date after loop
+    last_date, last_price = prices_series.index[-1], prices_series.iat[-1]
+    prev_support_dates[last_date], prev_support_prices[last_date] = _get_prev_support(last_price)
+    prev_resistance_dates[last_date], prev_resistance_prices[last_date] = _get_prev_resistance(last_price)
+
+    return prev_support_dates, prev_support_prices, prev_resistance_dates, prev_resistance_prices
 
 
 def variance(prices_df: pd.DataFrame, timeperiod=5) -> pd.Series:
--- fund-analyser-compute/lib/indicators/indicator_utils_test.py
+++ fund-analyser-compute/lib/indicators/indicator_utils_test.py
@@ -8,81 +8,78 @@ from lib.indicators.indicator_utils import stability, support_resistance
 
 
 def test_support_resistance():
-    prices_df = pd.DataFrame(
-        [[101.0, 101.0],
-         [101.0, 102.0],
-         [103.0, 102.0],
-         [103.0, 103.0],
-         [102.0, 103.0],
-         [102.0, 102.0],
-         [101.0, 104.0],
-         [102.0, 104.0]],
-        index=pd.date_range(date(2001, 1, 1), date(2001, 1, 8)),
-        columns=["isin1", "isin2"]
-    )
-    expected_sr = pd.DataFrame(
-        [[0, 0],
-         [0, 0],
-         [0, 0],
-         [-1, 0],
-         [0, -1],
-         [0, 1],
-         [1, 0],
-         [0, 0]],
-        index=pd.date_range(date(2001, 1, 1), date(2001, 1, 8)),
-        columns=["isin1", "isin2"]
-    )
+    index = pd.date_range(date(2001, 1, 1), date(2001, 1, 13))
+    columns = ["isin1", "isin2"]
+    prices = [
+        [100.0, 100.0],
+        [100.0, 100.0],
+        [104.0, 96.0],
+        [104.0, 96.0],
+        [99.0, 101.0],
+        [99.0, 101.0],
+        [102.0, 98.0],
+        [102.0, 98.0],
+        [101.0, 99.0],
+        [101.0, 99.0],
+        [103.0, 97.0],
+        [103.0, 97.0],
+        [100.0, 100.0]
+    ]
+    # isin1: support at (6th: 99, 10th: 101), resistance at (4th: 104, 8th: 102, 12th: 103)
+    # isin2: support at (4th: 96, 8th: 98, 12th: 97), resistance at (6th: 101, 10th: 99)
+    supports = [
+        [(pd.NaT, np.nan), (pd.NaT, np.nan)],
+        [(pd.NaT, np.nan), (pd.NaT, np.nan)],
+        [(pd.NaT, np.nan), (pd.NaT, np.nan)],
+        [(pd.NaT, np.nan), (pd.NaT, np.nan)],
+        [(pd.NaT, np.nan), (date(2001, 1, 4), 96.0)],
+        [(pd.NaT, np.nan), (date(2001, 1, 4), 96.0)],
+        [(date(2001, 1, 6), 99.0), (date(2001, 1, 4), 96.0)],
+        [(date(2001, 1, 6), 99.0), (date(2001, 1, 4), 96.0)],
+        [(date(2001, 1, 6), 99.0), (date(2001, 1, 8), 98.0)],
+        [(date(2001, 1, 6), 99.0), (date(2001, 1, 8), 98.0)],
+        [(date(2001, 1, 10), 101.0), (date(2001, 1, 4), 96.0)],
+        [(date(2001, 1, 10), 101.0), (date(2001, 1, 4), 96.0)],
+        [(date(2001, 1, 6), 99.0), (date(2001, 1, 12), 97.0)]
+    ]
+    resistances = [
+        [(pd.NaT, np.nan), (pd.NaT, np.nan)],
+        [(pd.NaT, np.nan), (pd.NaT, np.nan)],
+        [(pd.NaT, np.nan), (pd.NaT, np.nan)],
+        [(pd.NaT, np.nan), (pd.NaT, np.nan)],
+        [(date(2001, 1, 4), 104.0), (pd.NaT, np.nan)],
+        [(date(2001, 1, 4), 104.0), (pd.NaT, np.nan)],
+        [(date(2001, 1, 4), 104.0), (date(2001, 1, 6), 101.0)],
+        [(date(2001, 1, 4), 104.0), (date(2001, 1, 6), 101.0)],
+        [(date(2001, 1, 8), 102.0), (date(2001, 1, 6), 101.0)],
+        [(date(2001, 1, 8), 102.0), (date(2001, 1, 6), 101.0)],
+        [(date(2001, 1, 4), 104.0), (date(2001, 1, 10), 99.0)],
+        [(date(2001, 1, 4), 104.0), (date(2001, 1, 10), 99.0)],
+        [(date(2001, 1, 12), 103.0), (date(2001, 1, 6), 101.0)]
+    ]
+    prices_df = pd.DataFrame(prices, index=index, columns=columns)
     expected_prev_support_dates = pd.DataFrame(
-        [[pd.NaT, pd.NaT],
-         [pd.NaT, pd.NaT],
-         [pd.NaT, pd.NaT],
-         [pd.NaT, pd.NaT],
-         [pd.NaT, pd.NaT],
-         [pd.NaT, date(2001, 1, 6)],
-         [date(2001, 1, 7), date(2001, 1, 6)],
-         [date(2001, 1, 7), date(2001, 1, 6)]],
-        index=pd.date_range(date(2001, 1, 1), date(2001, 1, 8)),
-        columns=["isin1", "isin2"]
+        [[row[0][0], row[1][0]] for row in supports],
+        index=index,
+        columns=columns
     ).astype("datetime64[ns]")
     expected_prev_support_prices = pd.DataFrame(
-        [[np.nan, np.nan],
-         [np.nan, np.nan],
-         [np.nan, np.nan],
-         [np.nan, np.nan],
-         [np.nan, np.nan],
-         [np.nan, 102.0],
-         [101.0, 102.0],
-         [101.0, 102.0]],
-        index=pd.date_range(date(2001, 1, 1), date(2001, 1, 8)),
-        columns=["isin1", "isin2"]
+        [[row[0][1], row[1][1]] for row in supports],
+        index=index,
+        columns=columns
     )
     expected_prev_resistance_dates = pd.DataFrame(
-        [[pd.NaT, pd.NaT],
-         [pd.NaT, pd.NaT],
-         [pd.NaT, pd.NaT],
-         [date(2001, 1, 4), pd.NaT],
-         [date(2001, 1, 4), date(2001, 1, 5)],
-         [date(2001, 1, 4), date(2001, 1, 5)],
-         [date(2001, 1, 4), date(2001, 1, 5)],
-         [date(2001, 1, 4), date(2001, 1, 5)]],
-        index=pd.date_range(date(2001, 1, 1), date(2001, 1, 8)),
-        columns=["isin1", "isin2"]
+        [[row[0][0], row[1][0]] for row in resistances],
+        index=index,
+        columns=columns
     ).astype("datetime64[ns]")
     expected_prev_resistance_prices = pd.DataFrame(
-        [[np.nan, np.nan],
-         [np.nan, np.nan],
-         [np.nan, np.nan],
-         [103.0, np.nan],
-         [103.0, 103.0],
-         [103.0, 103.0],
-         [103.0, 103.0],
-         [103.0, 103.0]],
-        index=pd.date_range(date(2001, 1, 1), date(2001, 1, 8)),
-        columns=["isin1", "isin2"]
+        [[row[0][1], row[1][1]] for row in resistances],
+        index=index,
+        columns=columns
     )
-    sr, prev_support_dates, prev_support_prices, prev_resistance_dates, prev_resistance_prices \
+    prev_support_dates, prev_support_prices, prev_resistance_dates, prev_resistance_prices \
         = support_resistance(prices_df)
-    assert_frame_equal(sr, expected_sr)
     assert_frame_equal(prev_support_dates, expected_prev_support_dates)
     assert_frame_equal(prev_support_prices, expected_prev_support_prices)
     assert_frame_equal(prev_resistance_dates, expected_prev_resistance_dates)
