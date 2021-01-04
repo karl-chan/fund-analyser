2e7471db8a91980163b39580d98e2c522b7ea411
Karl Chan <karl-chan@bitbucket.org>
2021-01-04 17:05:52 +0000

    Remove ohlc

--- fund-analyser-compute/lib/stock/stock_cache.py
+++ fund-analyser-compute/lib/stock/stock_cache.py
@@ -15,10 +15,6 @@ _PICKLE_STOCK_CACHE = "stock_cache.pickle"
 
 _stock_cache: Dict[str, Stock] = dict()
 _prices_df: Optional[pd.DataFrame] = None
-_open_df: Optional[pd.DataFrame] = None
-_high_df: Optional[pd.DataFrame] = None
-_low_df: Optional[pd.DataFrame] = None
-_close_df: Optional[pd.DataFrame] = None
 _volume_df: Optional[pd.DataFrame] = None
 _expiration_time: Optional[datetime] = None
 _is_full: bool = False
@@ -41,10 +37,6 @@ def get_prices(symbols: Optional[Iterable[str]] = None) -> Tuple[
     maybe_initialise(symbols)
     symbols = _normalise_symbols(symbols)
     return (_prices_df[symbols],  # type: ignore
-            _open_df[symbols],  # type: ignore
-            _high_df[symbols],  # type: ignore
-            _low_df[symbols],  # type: ignore
-            _close_df[symbols],  # type: ignore
             _volume_df[symbols])  # type: ignore
 
 
@@ -93,16 +85,12 @@ def load_from_file(symbols: Optional[Iterable[str]]) -> None:
     """
     Loads stock cache from file if present and not expired, else raises ValueError.
     """
-    global _stock_cache, _prices_df, _open_df, _high_df, _low_df, _close_df, _volume_df, _expiration_time, _is_full
+    global _stock_cache, _prices_df, _volume_df, _expiration_time, _is_full
     data = read_from_disk(_PICKLE_STOCK_CACHE)
     if datetime.now() > data["expiry"]:
         raise ValueError(f"Stock cache expired at: {data['expiry']}")
-    _stock_cache, \
-    _prices_df, _open_df, _high_df, _low_df, _close_df, _volume_df, \
-    _expiration_time, _is_full = \
-        data["stocks"], \
-        data["prices_df"], data["open_df"], data["high_df"], data["low_df"], data["close_df"], data["volume_df"], \
-        data["expiry"], data["is_full"]
+    _stock_cache,  _prices_df, _volume_df,  _expiration_time, _is_full = \
+        data["stocks"], data["prices_df"], data["volume_df"], data["expiry"], data["is_full"]
     if not valid(symbols):
         raise ValueError(f"Stock cache needs refresh because it doesn't contain all symbols: {symbols}")
 
@@ -112,16 +100,10 @@ def save_to_file() -> None:
     Saves in-memory stock cache to file.
     :return:
     """
-    global _stock_cache, \
-        _prices_df, _open_df, _high_df, _low_df, _close_df, _volume_df, \
-        _expiration_time, _is_full
+    global _stock_cache, _prices_df, _volume_df, _expiration_time, _is_full
     write_to_disk(_PICKLE_STOCK_CACHE, {
         "stocks": _stock_cache,
         "prices_df": _prices_df,
-        "open_df": _open_df,
-        "high_df": _high_df,
-        "low_df": _low_df,
-        "close_df": _close_df,
         "volume_df": _volume_df,
         "expiry": _expiration_time,
         "is_full": _is_full
@@ -132,9 +114,7 @@ def refresh(symbols: Optional[Iterable[str]]) -> None:
     """
     Builds a fresh copy of stock cache from web.
     """
-    global _stock_cache, \
-        _prices_df, _open_df, _high_df, _low_df, _close_df, _volume_df, \
-        _expiration_time, _is_full
+    global _stock_cache, _prices_df, _volume_df, _expiration_time, _is_full
     log_info("Refreshing stock cache...")
     _stock_cache = dict()
     combined_prices = []
@@ -147,10 +127,8 @@ def refresh(symbols: Optional[Iterable[str]]) -> None:
         combined_prices.append(stock_stream_entry.historic_prices)
     log_debug("Merging stock historic prices...")
     _combined_df = pd.concat(combined_prices, axis=1).resample("B").asfreq().fillna(method="ffill")
-    _prices_df, _open_df, _high_df, _low_df, _close_df, _volume_df = \
-        _combined_df[["price"]], _combined_df[["open"]], _combined_df[["high"]], \
-        _combined_df[["low"]], _combined_df[["close"]], _combined_df[["volume"]]
-    _prices_df.columns = _open_df.columns = _high_df.columns = _low_df.columns = _close_df.columns = _volume_df.columns = _stock_cache.keys()
+    _prices_df, _volume_df = _combined_df[["price"]], _combined_df[["volume"]]
+    _prices_df.columns = _volume_df.columns = _stock_cache.keys()
     _expiration_time = _prices_df.last_valid_index() + EXPIRY
     _is_full = symbols is None
     save_to_file()
