d516c0a97bc915c32abc38034ebcecda40b0f4ae
Karl Chan <karl-chan@bitbucket.org>
2021-04-17 12:16:35 +0100

    Reduce trading212 memory usage

--- fund-analyser-data/lib/stock/Trading212.ts
+++ fund-analyser-data/lib/stock/Trading212.ts
@@ -1,4 +1,3 @@
-import cheerio from 'cheerio'
 import Http from '../util/http'
 import * as streamWrapper from '../util/streamWrapper'
 import { SymbolProvider } from './StockFactory'
@@ -9,11 +8,10 @@ export default class Trading212 implements SymbolProvider {
   async getSymbols () {
     const url = 'https://www.trading212.com/en/Trade-Equities?tab=isa'
     const { data } = await http.asyncGet(url)
-    const $ = cheerio.load(data)
-    const symbols = $('#all-equities > div[id^="equity-row-"]')
-      .filter((i, div) => $(div).find('> div[data-label="Market name"]').text().trim() === 'NASDAQ')
-      .map((i, div) => $(div).find('> div[data-label="Instrument"]').text().trim())
-      .get()
+    const symbols = Array.from(
+      (data as string)
+        .matchAll(/data-label="Instrument">([^<]+)<\/div>(?:.*\n){6}.*NASDAQ/g))
+      .map(group => group[1])
     return symbols
   }
 
