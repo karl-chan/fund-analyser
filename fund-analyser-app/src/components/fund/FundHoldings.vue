71bd08c2181839f8ae887f4441ea2e11eed1b94f
Karl Chan <karl-chan@bitbucket.org>
2021-09-15 22:26:19 +0100

    Fix QTable formatting

--- fund-analyser-app/src/components/fund/FundHoldings.vue
+++ fund-analyser-app/src/components/fund/FundHoldings.vue
@@ -1,17 +1,18 @@
 <template lang="pug">
 q-table(title="Real time information" :rows="rowData" :columns="columns" :pagination.sync="pagination" row-key="name"
         no-results-label="No information available" dense :hide-bottom="!!rowData.length")
-  q-tr(slot="body" slot-scope="props" :props="props")
-    q-td(key="name" :props="props" style="max-width: 75px; overflow: hidden; text-overflow: ellipsis")
-      |  {{ props.row.name }}
-      q-tooltip {{ props.row.name }}
-    q-td(key="todaysChange" :props="props" :class="{'text-green': props.row.todaysChange > 0, 'text-red': props.row.todaysChange < 0}")
-      | {{ $utils.format.formatPercentage(props.row.todaysChange) }}
-      q-btn(v-if="props.row.symbol" icon="info" @click="openURL('https://markets.ft.com/data/equities/tearsheet/summary?s=' + props.row.symbol)" color="primary" flat rounded dense)
-    q-td(key="weight" :props="props")
-      | {{ $utils.format.formatPercentage(props.row.weight) }}
-    q-td(key="currency" :props="props")
-      | {{ $utils.format.formatString(props.row.currency) }}
+  template(v-slot:body="props")
+    q-tr(:props="props")
+      q-td(key="name" :props="props" style="max-width: 75px; overflow: hidden; text-overflow: ellipsis")
+        |  {{ props.row.name }}
+        q-tooltip {{ props.row.name }}
+      q-td(key="todaysChange" :props="props" :class="{'text-green': props.row.todaysChange > 0, 'text-red': props.row.todaysChange < 0}")
+        | {{ $utils.format.formatPercentage(props.row.todaysChange) }}
+        q-btn(v-if="props.row.symbol" icon="info" @click="openURL('https://markets.ft.com/data/equities/tearsheet/summary?s=' + props.row.symbol)" color="primary" flat rounded dense)
+      q-td(key="weight" :props="props")
+        | {{ $utils.format.formatPercentage(props.row.weight) }}
+      q-td(key="currency" :props="props")
+        | {{ $utils.format.formatString(props.row.currency) }}
 </template>
 
 <script>
