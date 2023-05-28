3f2bc3df37cdb1d73c96d5a013366ee4287c2192
Karl Chan <karl-chan@bitbucket.org>
2023-05-28 22:40:13 +0100

    Relax FreeRealTime test to allow longestTimeGap up to 1 min

--- fund-analyser-data/lib/stock/FreeRealTime.test.ts
+++ fund-analyser-data/lib/stock/FreeRealTime.test.ts
@@ -96,7 +96,7 @@ describe('FreeRealTime', () => {
         estChange: expect.toBeNumber(),
         bidAskSpread: expect.toBePositive(),
         // apple is highly liquid stock, expect a trade every few seconds
-        longestTimeGap: expect.toBeWithin(0, 10)
+        longestTimeGap: expect.toBeWithin(0, 60)
       })
       expect(summary.fundamentals).toMatchObject({
         marketCap: expect.toBePositive(),
